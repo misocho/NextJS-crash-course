51d5842d719f40927505cbd6a82f85beb481a0d1
misocho <misochobrian@gmail.com>
2019-11-07 11:29:18 +0300

    Setup the app - Set up nextjs - Add the app`s layout - Add the app`s navigatin - Add bootstrap

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import Layout from "../components/Layout";
+
+const About = () => (
+  <Layout>
+    <div>
+      <h1>About BitzPrice</h1>
+      <p>Application to view bitcoin prices</p>
+    </div>
+  </Layout>
+);
+
+export default About;
